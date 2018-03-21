0324d03821b8f1aaeea18769f28120832f77062a
taran2525 <for-rega@ya.ru>
2018-03-21 22:08:21 +0400

    First commit

---
+++ assets/js/functions.js
@@ -0,0 +1,60 @@
+(function ($) {
+  'use strict';
+
+  $(document).ready(function () {
+
+  	function blueasyTabs(){
+  		var $wrapper = $('.tab-wrapper'),
+     $menu =    $wrapper.find('.tab-menu li'),
+     $content = $wrapper.find('.thumbs-row');
+
+
+     $content.not(':first-of-type').hide();
+     $content.each(function(i){
+      $(this).attr('data-tab', 'tab-'+i);
+    });
+     $menu.each(function(i){
+      $(this).attr('data-tab', 'tab-'+i);
+    });
+
+     $menu.click(function() {
+      var $getWrapper = $(this).closest($wrapper),
+      dataTab    = $(this).attr('data-tab');
+
+      $getWrapper.find($menu).find('a').removeClass('active');
+      $(this).find('a').addClass('active');
+
+      $getWrapper.find($content).hide();
+      $getWrapper.find($content).filter('[data-tab=' + dataTab + ']').show();	
+    });
+   }	
+   blueasyTabs();
+//end blueasyTabs
+
+
+//blueasyMenu
+
+
+
+
+function blueasyMenu() {
+      var $trigger = $('.trigger-nav'),
+          $menu = $('.trigger-victim');
+      
+      $trigger.click(function() {
+        $(this).next($menu).slideToggle();
+      });
+      
+      $(window).resize(function() {
+        if ( $(window).width() > 992 ) {
+          $menu.removeAttr('style');
+        }
+      });
+    }
+    blueasyMenu();
+
+
+
+  }); //end ready
+
+}(jQuery));
